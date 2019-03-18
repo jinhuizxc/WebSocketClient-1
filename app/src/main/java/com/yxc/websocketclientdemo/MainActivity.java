282f7df966410b55d02f209147e3e3c8a68f0ab4
yxc <xch_yang@126.com>
2019-03-18 23:36:58 +0800

    add message notification.

--- app/src/main/java/com/yxc/websocketclientdemo/MainActivity.java
+++ app/src/main/java/com/yxc/websocketclientdemo/MainActivity.java
@@ -1,10 +1,20 @@
 package com.yxc.websocketclientdemo;
 
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -22,6 +32,8 @@ import com.yxc.websocketclientdemo.im.JWebSocketClientService;
 import com.yxc.websocketclientdemo.modle.ChatMessage;
 import com.yxc.websocketclientdemo.util.Util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,11 +48,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_send;
     private List<ChatMessage> chatMessageList = new ArrayList<>();//消息列表
     private Adapter_ChatMessage adapter_chatMessage;
+    private ChatMessageReceiver chatMessageReceiver;
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            //服务与活动成功绑定
             Log.e("MainActivity", "服务与活动成功绑定");
             binder = (JWebSocketClientService.JWebSocketClientBinder) iBinder;
             jWebSClientService = binder.getService();
@@ -49,10 +61,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
-            //服务与活动断开
             Log.e("MainActivity", "服务与活动成功断开");
         }
     };
+
+    private class ChatMessageReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message=intent.getStringExtra("message");
+            ChatMessage chatMessage=new ChatMessage();
+            chatMessage.setContent(message);
+            chatMessage.setIsMeSend(0);
+            chatMessage.setIsRead(1);
+            chatMessage.setTime(System.currentTimeMillis()+"");
+            chatMessageList.add(chatMessage);
+            initChatMsgListView();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +90,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startJWebSClientService();
         //绑定服务
         bindService();
+        //注册广播
+        doRegisterReceiver();
+        //检测通知是否开启
+        checkNotification(mContext);
         findViewById();
         initView();
     }
@@ -81,6 +111,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void startJWebSClientService() {
         Intent intent = new Intent(mContext, JWebSocketClientService.class);
         startService(intent);
+    }
+    /**
+     * 动态注册广播
+     */
+    private void doRegisterReceiver() {
+        chatMessageReceiver = new ChatMessageReceiver();
+        IntentFilter filter = new IntentFilter("com.xch.servicecallback.content");
+        registerReceiver(chatMessageReceiver, filter);
     }
 
 
@@ -150,4 +188,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listView.setAdapter(adapter_chatMessage);
         listView.setSelection(chatMessageList.size());
     }
+
+
+    /**
+     * 检测是否开启通知
+     *
+     * @param context
+     */
+    private void checkNotification(final Context context) {
+        if (!isNotificationEnabled(context)) {
+            new AlertDialog.Builder(context).setTitle("温馨提示")
+                    .setMessage("你还未开启系统通知，将影响消息的接收，要去开启吗？")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setNotification(context);
+                        }
+                    }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            }).show();
+        }
+    }
+    /**
+     * 如果没有开启通知，跳转至设置界面
+     *
+     * @param context
+     */
+    private void setNotification(Context context) {
+        Intent localIntent = new Intent();
+        //直接跳转到应用通知设置的代码：
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            localIntent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+            localIntent.putExtra("app_package", context.getPackageName());
+            localIntent.putExtra("app_uid", context.getApplicationInfo().uid);
+        } else if (android.os.Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+            localIntent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            localIntent.addCategory(Intent.CATEGORY_DEFAULT);
+            localIntent.setData(Uri.parse("package:" + context.getPackageName()));
+        } else {
+            //4.4以下没有从app跳转到应用通知设置页面的Action，可考虑跳转到应用详情页面,
+            localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (Build.VERSION.SDK_INT >= 9) {
+                localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                localIntent.setData(Uri.fromParts("package", context.getPackageName(), null));
+            } else if (Build.VERSION.SDK_INT <= 8) {
+                localIntent.setAction(Intent.ACTION_VIEW);
+                localIntent.setClassName("com.android.settings", "com.android.setting.InstalledAppDetails");
+                localIntent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+            }
+        }
+        context.startActivity(localIntent);
+    }
+
+    /**
+     * 获取通知权限,监测是否开启了系统通知
+     *
+     * @param context
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private boolean isNotificationEnabled(Context context) {
+
+        String CHECK_OP_NO_THROW = "checkOpNoThrow";
+        String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+
+        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        ApplicationInfo appInfo = context.getApplicationInfo();
+        String pkg = context.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+
+        Class appOpsClass = null;
+        try {
+            appOpsClass = Class.forName(AppOpsManager.class.getName());
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE,
+                    String.class);
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+
+            int value = (Integer) opPostNotificationValue.get(Integer.class);
+            return ((Integer) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/yxc/websocketclientdemo/im/JWebSocketClientService.java
+++ app/src/main/java/com/yxc/websocketclientdemo/im/JWebSocketClientService.java
@@ -1,16 +1,28 @@
 package com.yxc.websocketclientdemo.im;
 
+import android.annotation.SuppressLint;
+import android.app.KeyguardManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.yxc.websocketclientdemo.MainActivity;
+import com.yxc.websocketclientdemo.R;
 import com.yxc.websocketclientdemo.util.Util;
 
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+
+import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
 
 public class JWebSocketClientService extends Service {
     private URI uri;
@@ -24,6 +36,7 @@ public class JWebSocketClientService extends Service {
             return JWebSocketClientService.this;
         }
     }
+
     @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
@@ -59,7 +72,14 @@ public class JWebSocketClientService extends Service {
         client = new JWebSocketClient(uri) {
             @Override
             public void onMessage(String message) {
-                Log.e("JWebSocketClientService", "收到的消息："+message);
+                Log.e("JWebSocketClientService", "收到的消息：" + message);
+
+                Intent intent = new Intent();
+                intent.setAction("com.xch.servicecallback.content");
+                intent.putExtra("message", message);
+                sendBroadcast(intent);
+
+                checkLockAndShowNotification(message);
             }
 
             @Override
@@ -91,11 +111,12 @@ public class JWebSocketClientService extends Service {
 
     /**
      * 发送消息
+     *
      * @param msg
      */
     public void sendMsg(String msg) {
         if (null != client) {
-            Log.e("JWebSocketClientService", "发送的消息："+msg);
+            Log.e("JWebSocketClientService", "发送的消息：" + msg);
             client.send(msg);
         }
     }
@@ -114,4 +135,58 @@ public class JWebSocketClientService extends Service {
             client = null;
         }
     }
+
+
+//    -----------------------------------消息通知--------------------------------------------------------
+
+    /**
+     * 检查锁屏状态，如果锁屏先点亮屏幕
+     *
+     * @param content
+     */
+    private void checkLockAndShowNotification(String content) {
+        //管理锁屏的一个服务
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        if (km.inKeyguardRestrictedInputMode()) {//锁屏
+            //获取电源管理器对象
+            PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+            if (!pm.isScreenOn()) {
+                @SuppressLint("InvalidWakeLockTag") PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP |
+                        PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "bright");
+                wl.acquire();  //点亮屏幕
+                wl.release();  //任务结束后释放
+            }
+            sendNotification(content);
+        } else {
+            sendNotification(content);
+        }
+    }
+
+    /**
+     * 发送通知
+     *
+     * @param content
+     */
+    private void sendNotification(String content) {
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager notifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                // 设置该通知优先级
+                .setPriority(Notification.PRIORITY_MAX)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("昵称")
+                .setContentText(content)
+                .setVisibility(VISIBILITY_PUBLIC)
+                .setWhen(System.currentTimeMillis())
+                // 向通知添加声音、闪灯和振动效果
+                .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_ALL | Notification.DEFAULT_SOUND)
+                .setContentIntent(pendingIntent)
+                .build();
+        notifyManager.notify(1, notification);//id要保证唯一
+    }
+
+
 }
