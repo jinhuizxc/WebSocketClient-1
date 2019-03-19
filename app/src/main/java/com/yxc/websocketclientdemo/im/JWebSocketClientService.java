c3fd605b6f5fd662beac4f1caee603efdc76c51b
yxc <xch_yang@126.com>
2019-03-19 15:34:38 +0800

    serivce Keep alive.

--- app/src/main/java/com/yxc/websocketclientdemo/im/JWebSocketClientService.java
+++ app/src/main/java/com/yxc/websocketclientdemo/im/JWebSocketClientService.java
@@ -9,6 +9,8 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
@@ -25,10 +27,39 @@ import java.net.URI;
 import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
 
 public class JWebSocketClientService extends Service {
-    private URI uri;
     public JWebSocketClient client;
-    private static final int STATUS_MESSAGE = 0x00;
     private JWebSocketClientBinder mBinder = new JWebSocketClientBinder();
+    private final static int GRAY_SERVICE_ID = 1001;
+    //灰色保活
+    public static class GrayInnerService extends Service {
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            startForeground(GRAY_SERVICE_ID, new Notification());
+            stopForeground(true);
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+    }
+    PowerManager.WakeLock wakeLock;//锁屏唤醒
+    //获取电源锁，保持该服务在屏幕熄灭时仍然获取CPU时，保持运行
+    @SuppressLint("InvalidWakeLockTag")
+    private void acquireWakeLock()
+    {
+        if (null == wakeLock)
+        {
+            PowerManager pm = (PowerManager)this.getSystemService(Context.POWER_SERVICE);
+            wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK|PowerManager.ON_AFTER_RELEASE, "PostLocationService");
+            if (null != wakeLock)
+            {
+                wakeLock.acquire();
+            }
+        }
+    }
 
     //用于Activity和service通讯
     public class JWebSocketClientBinder extends Binder {
@@ -51,8 +82,26 @@ public class JWebSocketClientService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         //初始化websocket
         initSocketClient();
-        return super.onStartCommand(intent, flags, startId);
+        mHandler.postDelayed(heartBeatRunnable, HEART_BEAT_RATE);//开启心跳检测
+
+        //设置service为前台服务，提高优先级
+        if (Build.VERSION.SDK_INT < 18) {
+            //Android4.3以下 ，隐藏Notification上的图标
+            startForeground(GRAY_SERVICE_ID, new Notification());
+        } else if(Build.VERSION.SDK_INT>18 && Build.VERSION.SDK_INT<25){
+            //Android4.3 - Android7.0，隐藏Notification上的图标
+            Intent innerIntent = new Intent(this, GrayInnerService.class);
+            startService(innerIntent);
+            startForeground(GRAY_SERVICE_ID, new Notification());
+        }else{
+            //Android7.0以上app启动后通知栏会出现一条"正在运行"的通知
+            startForeground(GRAY_SERVICE_ID, new Notification());
+        }
+
+        acquireWakeLock();
+        return START_STICKY;
     }
+
 
     @Override
     public void onDestroy() {
@@ -68,7 +117,7 @@ public class JWebSocketClientService extends Service {
      * 初始化websocket连接
      */
     private void initSocketClient() {
-        uri = URI.create(Util.ws);
+        URI uri = URI.create(Util.ws);
         client = new JWebSocketClient(uri) {
             @Override
             public void onMessage(String message) {
@@ -189,4 +238,42 @@ public class JWebSocketClientService extends Service {
     }
 
 
+    //    -------------------------------------websocket心跳检测------------------------------------------------
+    private static final long HEART_BEAT_RATE = 10 * 1000;//每隔10秒进行一次对长连接的心跳检测
+    private Handler mHandler = new Handler();
+    private Runnable heartBeatRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.e("JWebSClientService", "心跳包检测websocket连接状态");
+            if (client != null) {
+                if (client.isClosed()) {
+                    reconnectWs();
+                }
+            } else {
+                //如果client已为空，重新初始化连接
+                client = null;
+                initSocketClient();
+            }
+            //每隔一定的时间，对长连接进行一次心跳检测
+            mHandler.postDelayed(this, HEART_BEAT_RATE);
+        }
+    };
+
+    /**
+     * 开启重连
+     */
+    private void reconnectWs() {
+        mHandler.removeCallbacks(heartBeatRunnable);
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Log.e("JWebSClientService", "开启重连");
+                    client.reconnectBlocking();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
 }
